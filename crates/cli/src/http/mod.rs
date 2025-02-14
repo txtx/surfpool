83ba148f1f555359b92bb22111fb9652c88c28b3
MicaiahReid <micaiahreid@gmail.com>
2025-02-14 09:18:43 -0500

    fix(cli): get gql server responding

--- crates/cli/src/http/mod.rs
+++ crates/cli/src/http/mod.rs
@@ -3,9 +3,9 @@ use actix_cors::Cors;
 use actix_web::dev::ServerHandle;
 use actix_web::http::header::{self};
 use actix_web::web::{self, Data};
-use actix_web::Error;
+use actix_web::HttpRequest;
 use actix_web::{middleware, App, HttpResponse, HttpServer};
-use actix_web::{HttpRequest, Responder};
+use actix_web::{Error, Responder};
 use crossbeam::channel::{Receiver, Sender};
 use juniper_actix::{graphiql_handler, graphql_handler, playground_handler, subscriptions};
 use juniper_graphql_ws::ConnectionConfig;
@@ -15,7 +15,6 @@ use std::time::Duration;
 use surfpool_core::types::{Collection, SubgraphCommand, SubgraphEvent, SurfpoolConfig};
 use surfpool_gql::types::collection::CollectionData;
 use surfpool_gql::{new_graphql_schema, Context as GqlContext, GqlSchema};
-use txtx_core::kit::types::frontend::{ClientType, DiscoveryResponse};
 use txtx_core::kit::uuid::Uuid;
 
 #[cfg(feature = "explorer")]
@@ -138,9 +137,12 @@ async fn post_graphql(
     req: HttpRequest,
     payload: web::Payload,
     schema: Data<GqlSchema>,
-    context: Data<GqlContext>,
+    context: Data<RwLock<GqlContext>>,
 ) -> Result<HttpResponse, Error> {
     println!("POST /graphql");
+    let context = context
+        .read()
+        .map_err(|_| actix_web::error::ErrorInternalServerError("Failed to read context"))?;
     graphql_handler(&schema, &context, req, payload).await
 }
 
@@ -148,9 +150,12 @@ async fn get_graphql(
     req: HttpRequest,
     payload: web::Payload,
     schema: Data<GqlSchema>,
-    context: Data<GqlContext>,
+    context: Data<RwLock<GqlContext>>,
 ) -> Result<HttpResponse, Error> {
     println!("GET /graphql");
+    let context = context
+        .read()
+        .map_err(|_| actix_web::error::ErrorInternalServerError("Failed to read context"))?;
     graphql_handler(&schema, &context, req, payload).await
 }
 
@@ -158,8 +163,11 @@ async fn subscriptions(
     req: HttpRequest,
     stream: web::Payload,
     schema: Data<GqlSchema>,
-    context: Data<GqlContext>,
+    context: Data<RwLock<GqlContext>>,
 ) -> Result<HttpResponse, Error> {
+    let context = context
+        .read()
+        .map_err(|_| actix_web::error::ErrorInternalServerError("Failed to read context"))?;
     let ctx = GqlContext {
         collections_store: context.collections_store.clone(),
         entries_broadcaster: context.entries_broadcaster.clone(),
