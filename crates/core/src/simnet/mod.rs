bbb0568cbc8fed8f1c312c0987661aa0725a4d67
MicaiahReid <micaiahreid@gmail.com>
2025-03-06 01:15:28 -0500

    refactor(core): extract fn to start rpc server to better handle errors

--- crates/core/src/simnet/mod.rs
+++ crates/core/src/simnet/mod.rs
@@ -107,38 +107,13 @@ pub async fn start(
     let (plugin_manager_commands_tx, plugin_manager_commands_rx) = unbounded();
 
     let context = Arc::new(RwLock::new(context));
-    let middleware = SurfpoolMiddleware {
-        context: context.clone(),
-        simnet_commands_tx: simnet_commands_tx.clone(),
-        plugin_manager_commands_tx,
-        config: config.rpc.clone(),
-    };
-
-    let simnet_events_tx_copy = simnet_events_tx.clone();
-    let server_bind: SocketAddr = config.rpc.get_socket_address().parse()?;
-
-    let mut io = MetaIoHandler::with_middleware(middleware);
-    io.extend_with(rpc::minimal::SurfpoolMinimalRpc.to_delegate());
-    io.extend_with(rpc::full::SurfpoolFullRpc.to_delegate());
-    io.extend_with(rpc::accounts_data::SurfpoolAccountsDataRpc.to_delegate());
-    io.extend_with(rpc::accounts_scan::SurfpoolAccountsScanRpc.to_delegate());
-    io.extend_with(rpc::bank_data::SurfpoolBankDataRpc.to_delegate());
-
-    if !config.plugin_config_path.is_empty() {
-        io.extend_with(rpc::admin::SurfpoolAdminRpc.to_delegate());
-    }
-
-    let _handle = hiro_system_kit::thread_named("rpc handler").spawn(move || {
-        let server = ServerBuilder::new(io)
-            .cors(DomainsValidation::Disabled)
-            .start_http(&server_bind)
-            .unwrap();
-        let _ = simnet_events_tx_copy.send(SimnetEvent::Ready);
-        let _ = simnet_events_tx_copy.send(SimnetEvent::EpochInfoUpdate(epoch_info));
-
-        server.wait();
-        let _ = simnet_events_tx_copy.send(SimnetEvent::Shutdown);
-    });
+    let (plugin_manager_commands_rx, _rpc_handle) = start_rpc_server_thread(
+        &config,
+        &simnet_events_tx,
+        &simnet_commands_tx,
+        context.clone(),
+        &epoch_info,
+    )?;
 
     let simnet_config = config.simnet.clone();
     let simnet_events_tx_copy = simnet_events_tx.clone();
@@ -525,4 +500,240 @@ pub async fn start(
         let _ = simnet_events_tx.send(SimnetEvent::ClockUpdate(clock.clone()));
         ctx.svm.set_sysvar(&clock);
     }
+}
+
+fn start_geyser_plugin_thread(
+    plugin_manager_commands_rx: Receiver<PluginManagerCommand>,
+    subgraph_commands_tx: Sender<SubgraphCommand>,
+    simnet_events_tx: Sender<SimnetEvent>,
+    plugins_data_rx: Receiver<(Transaction, TransactionMetadata)>,
+) -> Result<JoinHandle<Result<(), String>>, String> {
+    let handle = hiro_system_kit::thread_named("Geyser Plugins Handler").spawn(move || {
+        let mut plugin_manager = GeyserPluginManager::new();
+
+        let ipc_router = RouterProxy::new();
+        // Note:
+        // At the moment, surfpool-subgraph is the only plugin that we're mounting.
+        // Please open an issue http://github.com/txtx/surfpool/issues/new if this is a feature you need!
+        //
+        // Proof of concept:
+        //
+        // let geyser_plugin_config_file = PathBuf::from("../../surfpool_subgraph_plugin.json");
+        // let contents = "{\"name\": \"surfpool-subgraph\", \"libpath\": \"target/release/libsurfpool_subgraph.dylib\"}";
+        // let result: serde_json::Value = json5::from_str(&contents).unwrap();
+        // let libpath = result["libpath"]
+        //     .as_str()
+        //     .unwrap();
+        // let mut libpath = PathBuf::from(libpath);
+        // if libpath.is_relative() {
+        //     let config_dir = geyser_plugin_config_file.parent().ok_or_else(|| {
+        //         GeyserPluginManagerError::CannotOpenConfigFile(format!(
+        //             "Failed to resolve parent of {geyser_plugin_config_file:?}",
+        //         ))
+        //     }).unwrap();
+        //     libpath = config_dir.join(libpath);
+        // }
+        // let plugin_name = result["name"].as_str().map(|s| s.to_owned()).unwrap_or(format!("surfpool-subgraph"));
+        // let (plugin, lib) = unsafe {
+        //     let lib = match Library::new(&surfpool_subgraph_path) {
+        //         Ok(lib) => lib,
+        //         Err(e) => {
+        //             let _ = simnet_events_tx_copy.send(SimnetEvent::ErrorLog(Local::now(), format!("Unable to load plugin {}: {}", plugin_name, e.to_string())));
+        //             continue;
+        //         }
+        //     };
+        //     let constructor: Symbol<PluginConstructor> = lib
+        //         .get(b"_create_plugin")
+        //         .map_err(|e| format!("{}", e.to_string()))?;
+        //     let plugin_raw = constructor();
+        //     (Box::from_raw(plugin_raw), lib)
+        // };
+
+        let plugin_name = "surfpool-subgraph";
+        // let surfpool_subgraph_path = Path::new("/tmp/surfpool-subgraph.dylib");
+        // Write the bytes to the file
+        // let mut surfpool_subgraph_file = File::create(surfpool_subgraph_path).unwrap();
+        // surfpool_subgraph_file.write_all(SUBGRAPH_PLUGIN_BYTES).unwrap();
+
+        let err = loop {
+            select! {
+                recv(plugin_manager_commands_rx) -> msg => {
+                    match msg {
+                        Ok(event) => {
+                            match event {
+                                PluginManagerCommand::LoadConfig(uuid, config, notifier) => {
+                                    let _ = subgraph_commands_tx.send(SubgraphCommand::CreateSubgraph(uuid.clone(), config.data.clone(), notifier));
+                                    let mut plugin = SurfpoolSubgraphPlugin::default();
+
+                                    let (server, ipc_token) = IpcOneShotServer::<IpcReceiver<SchemaDataSourcingEvent>>::new().expect("Failed to create IPC one-shot server.");
+                                    let subgraph_plugin_config = SubgraphPluginConfig {
+                                        uuid,
+                                        ipc_token,
+                                        subgraph_request: config.data.clone()
+                                    };
+                                    let config_file =match  serde_json::to_string(&subgraph_plugin_config) {
+                                        Ok(c) => c,
+                                        Err(e) => {
+                                            let _ = simnet_events_tx.send(SimnetEvent::error(format!("Failed to serialize subgraph plugin config: {:?}", e)));
+                                            continue;
+                                        }
+                                    };
+
+                                    if let Err(e) = plugin.on_load(&config_file, false) {
+                                        let _ = simnet_events_tx.send(SimnetEvent::error(format!("Failed to load Geyser plugin: {:?}", e)));
+                                    };
+                                    if let Ok((_, rx)) = server.accept() {
+                                        let subgraph_rx = ipc_router.route_ipc_receiver_to_new_crossbeam_receiver::<SchemaDataSourcingEvent>(rx);
+                                        let _ = subgraph_commands_tx.send(SubgraphCommand::ObserveSubgraph(subgraph_rx));
+                                    };
+                                    let plugin: Box<dyn GeyserPlugin> = Box::new(plugin);
+                                    // The approach is a bit hacky, but most likely temporary.
+                                    // To reduce the friction of iterating on our Geyser plugin,
+                                    // We are importing it as a crate, instead of dynamicly linking to it.
+                                    // We know we can use subgraph as a dylib, it's just too much friction for now.
+                                    let lib = unsafe {
+                                        #[cfg(target_os = "linux")]
+                                        let libm = Library::new("libm.so.6").unwrap(); // Common on Linux
+                                        #[cfg(target_os = "macos")]
+                                        let libm = Library::new("libm.dylib").unwrap(); // Common on macOS
+                                        #[cfg(target_os = "windows")]
+                                        let libm = Library::new("msvcrt.dll").unwrap(); // Math functions are in msvcrt.dll on Windows
+                                        libm
+                                    };
+                                    plugin_manager.plugins.push(LoadedGeyserPlugin::new(lib, plugin, Some(plugin_name.to_string())));
+                                    let _ = simnet_events_tx.send(SimnetEvent::PluginLoaded("surfpool-subgraph".into()));
+                                }
+                            }
+                        },
+                        Err(e) => {
+                            break format!("Failed to read plugin manager command: {:?}", e);
+                        },
+                    }
+                },
+                recv(plugins_data_rx) -> msg => match msg {
+                    Err(e) => {
+                        break format!("Failed to read new transaction to send to Geyser plugin: {e}");
+                    },
+                    Ok((transaction, transaction_metadata)) => {
+                        let mut inner_instructions = vec![];
+                        for (i,inner) in transaction_metadata.inner_instructions.iter().enumerate() {
+                            inner_instructions.push(
+                                InnerInstructions {
+                                    index: i as u8,
+                                    instructions: inner.iter().map(|i| InnerInstruction {
+                                        instruction: i.instruction.clone(),
+                                        stack_height: Some(i.stack_height as u32)
+                                    }).collect()
+                                }
+                            )
+                        }
+
+                        let transaction_status_meta = TransactionStatusMeta {
+                            status: Ok(()),
+                            fee: 0,
+                            pre_balances: vec![],
+                            post_balances: vec![],
+                            inner_instructions: Some(inner_instructions),
+                            log_messages: Some(transaction_metadata.logs.clone()),
+                            pre_token_balances: None,
+                            post_token_balances: None,
+                            rewards: None,
+                            loaded_addresses: LoadedAddresses {
+                                writable: vec![],
+                                readonly: vec![],
+                            },
+                            return_data: Some(transaction_metadata.return_data.clone()),
+                            compute_units_consumed: Some(transaction_metadata.compute_units_consumed),
+                        };
+
+                        let transaction = match SanitizedTransaction::try_from_legacy_transaction(transaction, &HashSet::new()) {
+                            Ok(tx) => tx,
+                            Err(e) => {
+                                let _ = simnet_events_tx.send(SimnetEvent::error(format!("Failed to notify Geyser plugin of new transaction: failed to serialize transaction: {:?}", e)));
+                                continue;
+                            }
+                        };
+
+                        let transaction_replica = ReplicaTransactionInfoV2 {
+                            signature: &transaction_metadata.signature,
+                            is_vote: false,
+                            transaction: &transaction,
+                            transaction_status_meta: &transaction_status_meta,
+                            index: 0
+                        };
+                        for plugin in plugin_manager.plugins.iter() {
+                            if let Err(e) = plugin.notify_transaction(ReplicaTransactionInfoVersions::V0_0_2(&transaction_replica), 0) {
+                                let _ = simnet_events_tx.send(SimnetEvent::error(format!("Failed to notify Geyser plugin of new transaction: {:?}", e)));
+                            };
+                        }
+                    }
+                }
+            }
+        };
+        Err(err)
+    }).map_err(|e| format!("Failed to spawn Geyser Plugins Handler thread: {:?}", e))?;
+    Ok(handle)
+}
+
+fn start_rpc_server_thread(
+    config: &SurfpoolConfig,
+    simnet_events_tx: &Sender<SimnetEvent>,
+    simnet_commands_tx: &Sender<SimnetCommand>,
+    context: Arc<RwLock<GlobalState>>,
+    epoch_info: &EpochInfo,
+) -> Result<(Receiver<PluginManagerCommand>, JoinHandle<()>), String> {
+    let (plugin_manager_commands_tx, plugin_manager_commands_rx) = unbounded();
+
+    let middleware = SurfpoolMiddleware::new(
+        context,
+        simnet_commands_tx,
+        &plugin_manager_commands_tx,
+        &config.rpc,
+    );
+    let server_bind: SocketAddr = config
+        .rpc
+        .get_socket_address()
+        .parse::<SocketAddr>()
+        .map_err(|e| e.to_string())?;
+
+    let simnet_events_tx = simnet_events_tx.clone();
+    let epoch_info = epoch_info.clone();
+
+    let mut io = MetaIoHandler::with_middleware(middleware);
+    io.extend_with(rpc::minimal::SurfpoolMinimalRpc.to_delegate());
+    io.extend_with(rpc::full::SurfpoolFullRpc.to_delegate());
+    io.extend_with(rpc::accounts_data::SurfpoolAccountsDataRpc.to_delegate());
+    io.extend_with(rpc::accounts_scan::SurfpoolAccountsScanRpc.to_delegate());
+    io.extend_with(rpc::bank_data::SurfpoolBankDataRpc.to_delegate());
+
+    if !config.plugin_config_path.is_empty() {
+        io.extend_with(rpc::admin::SurfpoolAdminRpc.to_delegate());
+    }
+
+    let _ = std::net::TcpListener::bind(server_bind)
+        .map_err(|e| format!("Failed to start RPC server: {}", e))?;
+
+    let _handle = hiro_system_kit::thread_named("RPC Handler")
+        .spawn(move || {
+            let server = match ServerBuilder::new(io)
+                .cors(DomainsValidation::Disabled)
+                .start_http(&server_bind)
+            {
+                Ok(server) => server,
+                Err(e) => {
+                    let _ = simnet_events_tx.send(SimnetEvent::Aborted(format!(
+                        "Failed to start RPC server: {:?}",
+                        e
+                    )));
+                    return;
+                }
+            };
+            let _ = simnet_events_tx.send(SimnetEvent::Ready);
+            let _ = simnet_events_tx.send(SimnetEvent::EpochInfoUpdate(epoch_info));
+
+            server.wait();
+            let _ = simnet_events_tx.send(SimnetEvent::Shutdown);
+        })
+        .map_err(|e| format!("Failed to spawn RPC Handler thread: {:?}", e))?;
+    Ok((plugin_manager_commands_rx, _handle))
 }
