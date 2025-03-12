490051cb474ac2b60d84ed6f0dc3ab5fd9b5e78a
MicaiahReid <micaiahreid@gmail.com>
2025-03-12 12:44:57 -0400

    feat(scaffold): generate subgraph runbook

--- crates/cli/src/scaffold/typhoon.rs
+++ crates/cli/src/scaffold/typhoon.rs
@@ -1,8 +1,10 @@
 use crate::types::Framework;
 use txtx_core::kit::helpers::fs::FileLocation;
 
+use super::ProgramMetadata;
+
 pub fn try_get_programs_from_project(
     _base_location: FileLocation,
-) -> Result<Option<(Framework, Vec<String>)>, String> {
+) -> Result<Option<(Framework, Vec<ProgramMetadata>)>, String> {
     Ok(None)
 }
