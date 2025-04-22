4479d98564e9defad9490ec316f9c45793d9e375
MicaiahReid <micaiahreid@gmail.com>
2025-04-22 10:37:16 -0400

    chore(core/simnet): add SimnetEvent Sender to RunloopContext

--- crates/core/src/simnet/mod.rs
+++ crates/core/src/simnet/mod.rs
@@ -532,7 +532,8 @@ fn start_rpc_server_thread(
 
     let middleware = SurfpoolMiddleware::new(
         context,
-        simnet_commands_tx,
+        &simnet_commands_tx,
+        &simnet_events_tx,
         &plugin_manager_commands_tx,
         &config.rpc,
     );
