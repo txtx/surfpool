9a9a5e9b3102466110d268619405fae5e6410bb3
Anthony Anderson <fnanderson93@gmail.com>
2025-05-07 21:10:57 -0400

    fix: :bug: make building a little better (#90)
    Co-authored-by: Micaiah Reid <micaiahreid@gmail.com>

--- crates/cli/src/runbook/mod.rs
+++ crates/cli/src/runbook/mod.rs
@@ -245,7 +245,10 @@ pub async fn configure_supervised_execution(
     let (block_broadcaster, _) = tokio::sync::broadcast::channel(5);
     let block_store = Arc::new(RwLock::new(BTreeMap::new()));
     let (kill_loops_tx, kill_loops_rx) = channel::bounded(1);
+    #[cfg(feature = "supervisor_ui")]
     let (action_item_events_tx, action_item_events_rx) = tokio::sync::broadcast::channel(32);
+    #[cfg(not(feature = "supervisor_ui"))]
+    let (_, action_item_events_rx) = tokio::sync::broadcast::channel(32);
 
     let moved_block_tx = block_tx.clone();
     let moved_kill_loops_tx = kill_loops_tx.clone();
