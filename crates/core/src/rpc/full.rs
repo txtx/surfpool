a01aabe572494de4fcd4962adc21d43dea778d2a
chizor iwuh <thisischizor@gmail.com>
2025-06-18 13:00:48 +0100

    rpc get_max_shred_insert_slot

--- crates/core/src/rpc/full.rs
+++ crates/core/src/rpc/full.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{str::FromStr, sync::atomic::Ordering};
 
 use itertools::Itertools;
 use jsonrpc_core::{BoxFuture, Error, Result};
@@ -1426,8 +1426,11 @@ impl Full for SurfpoolFullRpc {
         not_implemented_err("get_max_retransmit_slot")
     }
 
-    fn get_max_shred_insert_slot(&self, _meta: Self::Metadata) -> Result<Slot> {
-        not_implemented_err("get_max_shred_insert_slot")
+    fn get_max_shred_insert_slot(&self, meta: Self::Metadata) -> Result<Slot> {
+        meta.with_svm_reader(move |svm_reader| {
+            svm_reader.max_slots.shred_insert.load(Ordering::Relaxed)
+        })
+        .map_err(Into::into)
     }
 
     fn request_airdrop(
@@ -2154,7 +2157,7 @@ impl Full for SurfpoolFullRpc {
 #[cfg(test)]
 mod tests {
 
-    use std::thread::JoinHandle;
+    use std::{sync::atomic::Ordering, thread::JoinHandle};
 
     use base64::{prelude::BASE64_STANDARD, Engine};
     use crossbeam_channel::Receiver;
@@ -2168,6 +2171,7 @@ mod tests {
     };
     use solana_native_token::LAMPORTS_PER_SOL;
     use solana_pubkey::Pubkey;
+    use solana_rpc::max_slots::MaxSlots;
     use solana_sdk::{instruction::Instruction, system_instruction};
     use solana_signer::Signer;
     use solana_system_interface::program as system_program;
@@ -3715,5 +3719,20 @@ mod tests {
 
         let expected_local: Vec<Slot> = (100..=120).collect();
         assert_eq!(result, expected_local, "Should return local blocks 100-120");
+    }
+
+    #[test]
+    fn test_get_shred_max_insert_slot() {
+        let setup = TestSetup::new(SurfpoolFullRpc);
+
+        let result = setup
+            .rpc
+            .get_max_shred_insert_slot(Some(setup.context))
+            .unwrap();
+
+        assert_eq!(
+            result,
+            MaxSlots::default().shred_insert.load(Ordering::Relaxed)
+        )
     }
 }
