39a7dce892ec4fa3d322a49876ee71ab1617e1b3
Ludo Galabru <ludo@txtx.builders>
2025-07-11 16:49:21 -0400

    fix: dead code

--- crates/db/src/schema.rs
+++ crates/db/src/schema.rs
@@ -1,16 +1,4 @@
 diesel::table! {
-    mcp (id) {
-        id -> Integer,
-        created_at -> Date,
-        updated_at -> Date,
-        name -> Text,
-        description -> Text,
-        command -> Text,
-        version -> Text,
-    }
-}
-
-diesel::table! {
     collections (id) {
         id -> Text,
         created_at -> Date,
