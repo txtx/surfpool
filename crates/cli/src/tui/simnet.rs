6a940219faffd966b4281ada0885aea5c0110233
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-09-05 11:27:41 -0300

    wip

--- crates/cli/src/tui/simnet.rs
+++ crates/cli/src/tui/simnet.rs
@@ -556,6 +556,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                                     Local::now(),
                                     format!("Runbook '{}' execution completed", runbook_id),
                                 ));
+                                let _ = app.simnet_commands_tx.send(SimnetCommand::SetInstructionProfiling(true));
                                 app.status_bar_message = None;
                             }
                         },
@@ -651,6 +652,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                         },
                         Err(_) => {
                             deployment_completed = true;
+                            let _ = app.simnet_commands_tx.send(SimnetCommand::SetInstructionProfiling(true));
                         }
                     },
                 }
