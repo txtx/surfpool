813d255d267d37626dcf62e19dc37ce4cd898aca
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-09-15 21:15:31 -0300

    refactor(surfnet): handle default accounts in get_account_local and get_multiple_accounts_local methods

--- crates/core/src/surfnet/locker.rs
+++ crates/core/src/surfnet/locker.rs
@@ -226,11 +226,18 @@ impl SurfnetSvmLocker {
     pub fn get_account_local(&self, pubkey: &Pubkey) -> SvmAccessContext<GetAccountResult> {
         self.with_contextualized_svm_reader(|svm_reader| {
             match svm_reader.inner.get_account(pubkey) {
-                Some(account) => GetAccountResult::FoundAccount(
-                    *pubkey, account,
-                    // mark as not an account that should be updated in the SVM, since this is a local read and it already exists
-                    false,
-                ),
+                Some(account) => {
+                    //TODO: when LiteSVM updates `set_account` to remove accounts if 0 lamports, we can remove this check because the account will be removed from the store
+                    if account.eq(&Account::default()) {
+                        // If the account is default, it means it was deleted but still exists in our litesvm store
+                        return GetAccountResult::None(*pubkey);
+                    }
+                    GetAccountResult::FoundAccount(
+                        *pubkey, account,
+                        // mark as not an account that should be updated in the SVM, since this is a local read and it already exists
+                        false,
+                    )
+                }
                 None => match svm_reader.get_account_from_feature_set(pubkey) {
                     Some(account) => GetAccountResult::FoundAccount(
                         *pubkey, account,
@@ -282,7 +289,6 @@ impl SurfnetSvmLocker {
             _ => Ok(result),
         }
     }
-
     /// Retrieves multiple accounts from local cache, returning a contextualized result.
     pub fn get_multiple_accounts_local(
         &self,
@@ -293,11 +299,19 @@ impl SurfnetSvmLocker {
 
             for pubkey in pubkeys {
                 let res = match svm_reader.inner.get_account(pubkey) {
-                    Some(account) => GetAccountResult::FoundAccount(
-                        *pubkey, account,
-                        // mark as not an account that should be updated in the SVM, since this is a local read and it already exists
-                        false,
-                    ),
+                    Some(account) => {
+                        //TODO: when LiteSVM updates `set_account` to remove accounts if 0 lamports, we can remove this check because the account will be removed from the store
+                        if account.eq(&Account::default()) {
+                            // If the account is default, it means it was deleted but still exists in our litesvm store
+                            GetAccountResult::None(*pubkey)
+                        } else {
+                            GetAccountResult::FoundAccount(
+                                *pubkey, account,
+                                // mark as not an account that should be updated in the SVM, since this is a local read and it already exists
+                                false,
+                            )
+                        }
+                    }
                     None => match svm_reader.get_account_from_feature_set(pubkey) {
                         Some(account) => GetAccountResult::FoundAccount(
                             *pubkey, account,
