89216994800835a6cd504244362f5f297cf8d2d9
Ludo Galabru <ludo@txtx.builders>
2025-10-02 05:59:37 -0400

    fix: db deps

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,3 +1,9 @@
 pub use diesel;
 pub use diesel_dynamic_schema;
 pub mod schema;
+
+#[cfg(feature = "sqlite")]
+pub use diesel::sqlite;
+
+#[cfg(feature = "postgres")]
+pub use diesel::pg as postgres;
