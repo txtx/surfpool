81b247364489b7ecd11c6fff76fd89ced1de73af
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2025-11-26 14:53:15 +0530

    fix: variable name changes

--- crates/cli/src/cli/simnet/mod.rs
+++ crates/cli/src/cli/simnet/mod.rs
@@ -469,7 +469,7 @@ async fn write_and_execute_iac(
             }
         }
 
-        let input_location = cmd.runbook_input.clone();
+        let runbook_input = cmd.runbook_input.clone();
         // Is infrastructure-as-code (IaC) already setup?
         let base_location =
             FileLocation::from_path_string(&cmd.manifest_path)?.get_parent_location()?;
@@ -511,7 +511,7 @@ async fn write_and_execute_iac(
             simnet_events_tx,
             &on_disk_runbook_data,
             &in_memory_runbook_data,
-            &input_location,
+            &runbook_input,
         );
 
         let simnet_events_tx = simnet_events_tx.clone();
@@ -525,7 +525,7 @@ async fn write_and_execute_iac(
         if cmd.watch {
             let _handle = hiro_system_kit::thread_named("Watch Filesystem")
                 .spawn(move || {
-                    let input_location = input_location.clone();
+                    let runbook_input = runbook_input.clone();
                     let mut target_path = base_location.clone();
                     let _ = target_path.append_path("target");
                     let _ = target_path.append_path("deploy");
@@ -574,7 +574,7 @@ async fn write_and_execute_iac(
                             &simnet_events_tx,
                             &on_disk_runbook_data,
                             &in_memory_runbook_data,
-                            &input_location,
+                            &runbook_input,
                         );
 
                         let _ = hiro_system_kit::nestable_block_on(join_all(futures));
@@ -592,7 +592,7 @@ fn assemble_runbook_execution_futures(
     simnet_events_tx: &Sender<SimnetEvent>,
     on_disk_runbook_data: &Option<(FileLocation, Vec<String>)>,
     in_memory_runbook_data: &Option<(String, RunbookSources, WorkspaceManifest)>,
-    input_location: &Vec<String>,
+    runbook_input: &Vec<String>,
 ) -> Vec<std::pin::Pin<Box<dyn std::future::Future<Output = Result<(), String>> + Send>>> {
     let mut futures: Vec<
         std::pin::Pin<Box<dyn std::future::Future<Output = Result<(), String>> + Send>>,
@@ -627,7 +627,7 @@ fn assemble_runbook_execution_futures(
                 {
                     let mut ec = ExecuteRunbook::default_localnet(&runbook_id_owned)
                         .with_manifest_path(file_location_owned.to_string());
-                    ec.inputs = input_location.clone();
+                    ec.inputs = runbook_input.clone();
                     ec
                 },
                 do_setup_logger,
