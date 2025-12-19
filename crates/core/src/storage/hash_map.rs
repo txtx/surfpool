1aa70b6bc3ea3732757cf4003024a5b0361cd833
MicaiahReid <micaiahreid@gmail.com>
2025-12-19 10:44:15 -0500

    implement Storage trait for HashMap

--- crates/core/src/storage/hash_map.rs
+++ crates/core/src/storage/hash_map.rs
@@ -1,32 +1,43 @@
-// pub use std::collections::HashMap;
-// use std::hash::Hash;
+use serde::{Deserialize, Serialize};
+pub use std::collections::HashMap;
+use std::hash::Hash;
 
-// impl<K, V> super::Storage<K, V> for HashMap<K, V>
-// where
-//     K: Eq + Hash,
-// {
-//     fn connect(database_url: Option<&str>) -> super::StorageResult<Self>
-//     where
-//         Self: Sized,
-//     {
-//         if database_url.is_some() {
-//             return Err(super::StorageError::InvalidConfiguration(
-//                 "HashMap storage does not support database URLs".to_string(),
-//             ));
-//         }
-//         Ok(HashMap::new())
-//     }
+impl<K, V> super::Storage<K, V> for HashMap<K, V>
+where
+    K: Serialize + for<'de> Deserialize<'de> + Clone + Send + Sync + 'static + std::cmp::Eq + Hash,
+    V: Serialize + for<'de> Deserialize<'de> + Clone + Send + Sync + 'static,
+{
+    fn store(&mut self, key: K, value: V) -> super::StorageResult<()> {
+        self.insert(key, value);
+        Ok(())
+    }
 
-//     fn store(&mut self, key: K, value: V) -> super::StorageResult<()> {
-//         self.insert(key, value);
-//         Ok(())
-//     }
+    fn clear(&mut self) -> super::StorageResult<()> {
+        self.clear();
+        Ok(())
+    }
 
-//     fn get(&self, key: &K) -> super::StorageResult<Option<&V>> {
-//         Ok(self.get(key))
-//     }
+    fn get(&self, key: &K) -> super::StorageResult<Option<V>> {
+        Ok(self.get(key).cloned())
+    }
 
-//     fn take(&mut self, key: &K) -> super::StorageResult<Option<V>> {
-//         Ok(self.remove(key))
-//     }
-// }
+    fn take(&mut self, key: &K) -> super::StorageResult<Option<V>> {
+        Ok(self.remove(key))
+    }
+
+    fn keys(&self) -> super::StorageResult<Vec<K>> {
+        Ok(self.keys().cloned().collect())
+    }
+
+    fn into_iter(&self) -> super::StorageResult<Box<dyn Iterator<Item = (K, V)> + '_>> {
+        Ok(Box::new(self.clone().into_iter()))
+    }
+
+    fn clone_box(&self) -> Box<dyn super::Storage<K, V>> {
+        Box::new(self.clone())
+    }
+
+    fn contains_key(&self, key: &K) -> super::StorageResult<bool> {
+        Ok(self.contains_key(key))
+    }
+}
