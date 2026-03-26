f229455178d9668cbd51a3ae7be3968e6e09f24e
Micaiah Reid <micaiahreid@gmail.com>
2026-03-25 20:42:06 -0400

    bump txtx crates

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,7 +1,6 @@
 pub use diesel;
-pub use diesel_dynamic_schema;
-
 #[cfg(feature = "postgres")]
 pub use diesel::pg as postgres;
 #[cfg(feature = "sqlite")]
 pub use diesel::sqlite;
+pub use diesel_dynamic_schema;
